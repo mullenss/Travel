2f7624161a603896c9b54e820cd61933e6607d67
Samia Mullen <syedamullen@live.com>
2020-06-15 23:28:49 -0400

    first commit

---
+++ src/test/java/com/ben/java/fizzbuzzing/AppTest.java
@@ -0,0 +1,22 @@
+package com.ben.java.fizzbuzzing;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        App app = new App();
+        app.main(null);
+        assertTrue( true );
+    }
+}
