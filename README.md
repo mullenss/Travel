e35cf63174218bc89dee5b8f3f1730963e5d680c
Samia Mullen <syedamullen@live.com>
2020-07-13 22:15:52 -0400

    The places that I would like to travel to with my beloved

--- README.md
+++ README.md
@@ -1,12 +1,3 @@
-# fizzlebuzzing
-De Rigeur Implementation of popular programming test, in Java
-
-
-Feeling kinda portable. Might run across multiple popular architectures later, idk.
-
-
-FizzBuzz Rules, from https://wiki.c2.com/?FizzBuzzTest
-
-Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz
-
+# Travel
+The places that I would like to  travel to with my lover a.k.a the beloved, Bibi, Benhamin Mulang, na na na nanananana
 
--- src/main/java/com/ben/java/fizzbuzzing/App.java
+++ src/main/java/com/ben/java/fizzbuzzing/App.java
@@ -8,15 +8,15 @@ public class App {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 100; i++) {
-            if (i % 5 == 0 & i % 3 == 0) {
-                System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
+        for (int i = 1; i = 12; /*i*/++) {
+            if (i % 1 ==  0) {
+                System.out.println("Jamaica");
+            } else if (i % 2 == 0) {
+                System.out.println("Bahamas");
             } else if (i % 5 == 0) {
-                System.out.println("Buzz");
+                System.out.println("Nice");
             } else {
-                System.out.println(i);
+                System.out.println("Home");
             }
 
         }
